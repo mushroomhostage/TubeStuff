7c31270d75238d03bfab251bf86347d82b88b758
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-04-30 18:53:01 -0700

    Fix packaging

--- package.sh
+++ package.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-rm -rf net immibis
+rm -rf net
 mkdir -p net/minecraft/server
-./compile.sh
 cp mod_*.class net/minecraft/server/
-zip -r immibis-core-tubestuff-mcpc125-r1.zip net/ immibis
+zip -r zimmibis-core-tubestuff-mcpc125-r1.zip net/ immibis/ dan200/
